e1026da8ca15fe57fcdc490cfa7a30c76486121f
HamzaFerwana <hamza_ferwana@hotmail.com>
2025-12-03 19:36:38 +0200

    edits to models

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,26 +2,24 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
-    name = models.CharField(max_length=255, blank=True)
+class Employee(AbstractUser):
+    fullName = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    country = models.CharField(max_length=255, blank=True, null=True)
-    city = models.CharField(max_length=255, blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
-    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    password = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    role = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["email"]
 
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
 
 class SubCategory(models.Model):
@@ -37,32 +35,13 @@ class SubCategory(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
-from django.db import models
-
-
-class Brand(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    image = models.ImageField(
-        upload_to="brands/",
-        blank=True,
-        null=True,
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
 class Product(models.Model):
     STATUS_CHOICES = [
-        ("open", "Open"),
-        ("closed", "Closed"),
+        ("active", "Active"),
+        ("inactive", "Inactive"),
     ]
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(
-        "Category",
-        on_delete=models.PROTECT,
-        related_name="products",
-    )
+    brand = models.CharField(max_length=60)
     sub_category = models.ForeignKey(
         "SubCategory",
         on_delete=models.PROTECT,
@@ -70,16 +49,8 @@ class Product(models.Model):
         blank=True,
         null=True,
     )
-    brand = models.ForeignKey(
-        "Brand",
-        on_delete=models.PROTECT,
-        related_name="products",
-        blank=True,
-        null=True,
-    )
     unit = models.CharField(max_length=16)
     sku = models.CharField(max_length=255, unique=True)
-    minimum_quantity = models.PositiveIntegerField(default=0)
     quantity = models.PositiveIntegerField(default=0)
     description = models.TextField(blank=True, null=True)
     status = models.CharField(
