08ac096acd831b434042a042a1267c502cf528c1
Faris Rami <105630035+Fizox@users.noreply.github.com>
2025-12-11 12:29:17 +0200

    deploy ready

--- inventory_management_system/settings.py
+++ inventory_management_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vgxw@tpyvw#02n9k&2z1ia(pn*34_19469(q4&f32bq51e#de-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.227.211.218']
 
 ACCOUNT_EMAIL_REQUIRED = True
 SITE_ID=2
@@ -92,12 +92,15 @@ WSGI_APPLICATION = "inventory_management_system.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inventory_db',
+        'USER': 'postgres_user',
+        'PASSWORD': 'secure_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
